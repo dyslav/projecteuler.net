b644272f026aa2f15e7fe098c20b7a5b03100682
Full Name <kurmunke@gmail.com>
2018-08-17 15:00:08 +0300

    Update function isPrimeNumber()

--- 7.php
+++ 7.php
@@ -2,7 +2,7 @@
 // Problem 7
 // By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 // What is the 10 001st prime number?
-$start = microtime(true);
+
 function isPrimeNumber($num){
      for ($i=2; $i < $num; $i++) {
           if($num % $i === 0) {
@@ -25,6 +25,4 @@ function primeNumber($num) {
 }
 
 echo primeNumber(10001); //104743
-
-echo '<br/>'.round(microtime(true) - $start, 4).' sec';
 ?>
