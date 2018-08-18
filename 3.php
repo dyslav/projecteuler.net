159bd9adcffc760573b63fd218df4ef332f2b9ba
Full Name <kurmunke@gmail.com>
2018-08-18 13:38:53 +0300

    Solving problem #3

--- 3.php
+++ 3.php
@@ -3,7 +3,6 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
 function isPrimeNumber($num){
-     $arr = array();
      for ($i=2; $i < $num; $i++) {
           if($num % $i === 0) {
                return false;
