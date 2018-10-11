d913c3fe4454bf133ff6d55705391bcf30f2a290
Full Name <kurmunke@gmail.com>
2018-10-11 23:51:20 +0300

    Check PSR for .php

--- 25.php
+++ 25.php
@@ -18,7 +18,6 @@
 // The 12th term, F12, is the first term to contain three digits.
 // What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 
-
 function fibonacci($num)
 {
     $arr = array(0, 1);
