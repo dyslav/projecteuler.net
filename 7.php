4eb73a0b1349c93d7c4f6704495acb46fae44801
Vladyslav <kurmunke@gmail.com>
2020-02-19 02:10:36 +0200

    Refactored 7.php

--- 7.php
+++ 7.php
@@ -3,8 +3,6 @@
 // By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 // What is the 10 001st prime number?
 
-$start = microtime(true);
-
 function isPrimeNumber($num)
 {
     $halfNum = ceil($num / 2);
@@ -30,7 +28,3 @@ function primeNumber($num)
 }
 
 echo primeNumber(10001); //104743
-
-$time = microtime(true) - $start;
-echo "\r\n";
-var_dump($time);
